4ecb92fcbb5ddafab2bb179934f5ae6145c47bfd
Dinh <dinh.tran@sap.com>
2022-08-23 18:58:58 -0700

    t

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         ),
         .testTarget(
             name: "SwiftyRSATest",
-            dependencies: ["Source"],
+            dependencies: [],
             path: "Tests")
     ]
 )
